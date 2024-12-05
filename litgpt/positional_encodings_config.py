235e58015d5ae0fb52ae90370f29cce66d57116e
rafiqkamel <rafiq.kamel@tum.de>
2024-12-05 14:37:02 +0100

    Update positional encodings configuration with new parameters for sinusoidal and magnetic laplace encodings

--- litgpt/positional_encodings_config.py
+++ litgpt/positional_encodings_config.py
@@ -1,3 +1,3 @@
-sinousidial_encodings_q = 10000
-magentic_laplace_encodings_q = 1e-2
-sinousidial_encodings_dim = 64
+sinousidial_encodings_q = 1000
+magentic_laplace_encodings_q = 1e-3
+sinousidial_encodings_dim = 128
