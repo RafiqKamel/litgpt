764aeb9a94ab0bfff43475e2a41fe6078d3bf164
rafiqkamel <rafiq.kamel@tum.de>
2024-12-09 12:52:07 +0100

    fix: apply unidecode to instruction for consistent formatting in generate_example function

--- litgpt/finetune/lora.py
+++ litgpt/finetune/lora.py
@@ -49,7 +49,7 @@ from litgpt.new_utils import (
     prepare_eigvecs_datapoint,
 )
 from litgpt.prompts import PromptStyle
-
+from unidecode import unidecode
 
 def setup(
     checkpoint_dir: Path,
@@ -528,6 +528,7 @@ def generate_example(
 
     instruction = "They rely on older citizens and more affluent citizens."
     graph_text = "0 1\n0 3\n1 2\n3 4\n4 5\n4 13\n5 6\n6 7\n6 9\n6 11\n7 8\n9 10\n11 12\n13 14\n14 15\n14 17\n14 19\n15 16\n17 18\n19 20\n"
+    instruction = unidecode(instruction)
     fabric.print(instruction)
     prompt_style = eval.direction
     prompt_style_object = (
