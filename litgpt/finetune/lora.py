04d4292d6197895c2a0b70690b48d339ef52eeca
rafiqkamel <rafiq.kamel@tum.de>
2024-12-09 13:22:33 +0100

    fix: update instruction format in generate_example function for improved clarity

--- litgpt/finetune/lora.py
+++ litgpt/finetune/lora.py
@@ -526,7 +526,7 @@ def generate_example(
     fabric: L.Fabric, model: GPT, tokenizer: Tokenizer, eval: EvalArgs, data: DataModule
 ):
 
-    instruction = "They rely on older citizens and more affluent citizens."
+    instruction = "rely-01 :ARG0 they :ARG1 and :op1 have-degree-91 :ARG1 citizen :ARG2 old :ARG3 more :op2 have-degree-91 :ARG1 citizen :ARG2 affluence :ARG3 more"
     graph_text = "0 1\n0 3\n1 2\n3 4\n4 5\n4 13\n5 6\n6 7\n6 9\n6 11\n7 8\n9 10\n11 12\n13 14\n14 15\n14 17\n14 19\n15 16\n17 18\n19 20\n"
     instruction = unidecode(instruction)
     fabric.print(instruction)
